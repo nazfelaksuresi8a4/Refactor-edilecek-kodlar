44ba718d3a0db58a1b14d019e0795bbbe41cef31
İZBAN E22000 EMU <agmroc@gmail.com>
2025-06-22 16:05:15 +0300

    Create pyqtgraph hatası cozumu.cs

---
+++ pyqtgraph hatası cozumu.cs
@@ -0,0 +1,1 @@
+#pyqtgraph arrayleri destekler yani tek bir çıplak veri türünden diğer veri türüne örneğin .plot(0,1) gibi bir kullanım hatalı olur örnek kullanım bu reponun içindeki refactorlu vesiyonda mevcut
